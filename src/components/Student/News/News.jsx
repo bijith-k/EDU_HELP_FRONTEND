f70bcd433bbe70c9f0dfcc1dc99e638b7f798974
Bijith <bijithkatteeri011@gmail.com>
2023-05-07 21:09:49 +0530

    edited news

--- src/components/Student/News/News.jsx
+++ src/components/Student/News/News.jsx
@@ -14,135 +14,136 @@ const News = () => {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  
-
-  
-  
-
-    useEffect(() => {
-      console.log("first")
-      const fetchNews = async () => {
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
         const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?category=technology&country=in&apiKey=${
+          `https://newsdata.io/api/1/news?apikey=${
             import.meta.env.VITE_NEWS_API_KEY
-          }`
+          }&q=education&country=in&language=en&category=science,technology`
         );
-        console.log(response);
-        setNews(response.data.articles);
-        setLoading(false)
-      };
-      fetchNews();
-    }, []);
+        setNews(response.data.results);
+        setLoading(false);
+      } catch (error) {
+        
+        setLoading(false);
+      }
+    };
+    fetchNews();
+  }, []);
 
-    console.log(news)
+  
+  
+
+    
 const lastNewsIndex = currentPage * newsPerPage;
 const firstNewsIndex = lastNewsIndex - newsPerPage;
 
 const currentNews = news?.slice(firstNewsIndex, lastNewsIndex);
+ 
 
-   
-  return (
-    <div className="min-h-screen max-w-screen-2xl mx-auto w-full pt-16 bg-[#d4d8f0] overflow-x-hidden">
-      <Navbar />
-      <Header />
-      <HeadTitle title={"news"} />
-      {loading ? (
-        <Box
-          padding="6"
-          boxShadow="lg"
-          bg="white"
-          className="mt-5 mx-auto h-52 w-4/5 rounded-3xl flex md:flex-row flex-col  items-center  md:justify-center"
-        >
-          <Skeleton height="28" mt="3" width="28" />
-          <div className="ml-5 md:block hidden">
-            <Skeleton height="2" width={"2xl"} mt="3" />
-            <Skeleton height="2" width={"2xl"} mt="3" />
-            <Skeleton height="2" width={"2xl"} mt="3" />
-            <Skeleton height="2" width={"2xl"} mt="3" />
-          </div>
-          <div className="ml-5 md:hidden block">
-            <Skeleton height="2" width={"2xs"} mt="3" />
-            <Skeleton height="2" width={"2xs"} mt="3" />
-            <Skeleton height="2" width={"2xs"} mt="3" />
-            <Skeleton height="2" width={"2xs"} mt="3" />
-          </div>
-        </Box>
-      ) : (
-        <div>
-          {news?.length > 0 ? (
-            <div>
-              <div className="h-4/6">
-                {currentNews.map((news, index) => (
-                  <div className="p-2 flex justify-center ">
-                    <div className="bg-[#fffffe] hover:shadow-xl flex flex-col justify-evenly md:flex-row hover:opacity-90  text-[#232946]  w-3/4 rounded-xl mt-5 p-5 h-fit text-center">
-                      <div className=" w-36 h-36 bg-black flex flex-col mx-auto  md:mx-2">
-                        {news?.urlToImage ? (
-                          <img
-                            src={news.urlToImage}
-                            className="w-36  h-36 object-cover shadow-sm shadow-black"
-                            alt=""
-                          />
-                        ) : (
-                          <img
-                            src={newsImg}
-                            className="w-36  h-36 shadow-sm shadow-black"
-                            alt=""
-                          />
-                        )}
-                      </div>
-                      <div className="flex flex-col justify-center md:items-start uppercase">
-                        <div className="mb-4 font-bold">
-                          <p className="text-center">{news?.title}</p>
-                        </div>
-                        <div>
-                          {" "}
-                          <p className="text-start">{news?.description}</p>
-                        </div>
-                        <div>
-                          <p className="text-center">
-                            Source: {news?.source?.name}
-                          </p>
-                        </div>
-                      </div>
-                      <div className="flex flex-col justify-center m-2">
-                        <span>
-                          <a
-                            href={news?.url}
-                            className="bg-[#232946] p-2 text-[#fffffe] rounded-lg  uppercase font-bold mt-5 w-full hover:bg-slate-500 hover:text-white mx-auto"
-                          >
-                            View
-                          </a>{" "}
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
+   return (
+     <div className="min-h-screen max-w-screen-2xl mx-auto w-full pt-16 bg-[#d4d8f0] overflow-x-hidden">
+       <Navbar />
+       <Header />
+       <HeadTitle title={"news"} />
+       {loading ? (
+         <Box
+           padding="6"
+           boxShadow="lg"
+           bg="white"
+           className="mt-5 mx-auto h-52 w-4/5 rounded-3xl flex md:flex-row flex-col  items-center  md:justify-center"
+         >
+           <Skeleton height="28" mt="3" width="28" />
+           <div className="ml-5 md:block hidden">
+             <Skeleton height="2" width={"2xl"} mt="3" />
+             <Skeleton height="2" width={"2xl"} mt="3" />
+             <Skeleton height="2" width={"2xl"} mt="3" />
+             <Skeleton height="2" width={"2xl"} mt="3" />
+           </div>
+           <div className="ml-5 md:hidden block">
+             <Skeleton height="2" width={"2xs"} mt="3" />
+             <Skeleton height="2" width={"2xs"} mt="3" />
+             <Skeleton height="2" width={"2xs"} mt="3" />
+             <Skeleton height="2" width={"2xs"} mt="3" />
+           </div>
+         </Box>
+       ) : (
+         <div>
+           {news?.length > 0 ? (
+             <div>
+               <div className="h-4/6">
+                 {currentNews.map((news, index) => (
+                   <div className="p-2 flex justify-center ">
+                     <div className="bg-[#fffffe] hover:shadow-xl flex flex-col justify-evenly md:flex-row hover:opacity-90  text-[#232946]  w-3/4 rounded-xl mt-5 p-5 h-fit text-center">
+                       <div className=" md:w-3/12  flex justify-center items-center  mx-auto  md:mx-2">
+                         {news?.image_url ? (
+                           <img
+                             src={news.image_url}
+                             className="w-36  h-36 object-cover shadow-sm shadow-black"
+                             alt=""
+                           />
+                         ) : (
+                           <img
+                             src={newsImg}
+                             className="w-36  h-36 shadow-sm shadow-black"
+                             alt=""
+                           />
+                         )}
+                       </div>
+                       <div className="flex flex-col justify-center md:w-8/12 md:items-start uppercase">
+                         <div className="mb-4 font-bold">
+                           <p className="text-center">{news?.title}</p>
+                         </div>
+                         <div>
+                           {" "}
+                           <p className="text-start">{news?.description}</p>
+                         </div>
+                         <div>
+                           <p className="text-center">
+                             Source: {news?.source_id}
+                           </p>
+                         </div>
+                       </div>
+                       <div className="flex flex-col justify-center m-2 md:w-1/12">
+                         <span>
+                           <a
+                             href={news?.link}
+                             className="bg-[#232946] p-2 text-[#fffffe] rounded-lg  uppercase font-bold mt-5 w-full hover:bg-slate-500 hover:text-white mx-auto"
+                           >
+                             View
+                           </a>{" "}
+                         </span>
+                       </div>
+                     </div>
+                   </div>
+                 ))}
+               </div>
 
-              <Pagination
-                totalContents={news.length}
-                contentsPerPage={newsPerPage}
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}
-              />
-            </div>
-          ) : (
-            <div className="mt-5 h-40  ">
-              <p className="text-center font-bold text-xl mt-10">
-                No news is available at the moment
-              </p>
-              <p className="text-center font-semibold text-xl ">
-                Please check back later
-              </p>
-            </div>
-          )}
-        </div>
-      )}
-      <div className="mt-5">
-        <Footer />
-      </div>
-    </div>
-  );
+               <Pagination
+                 totalContents={news.length}
+                 contentsPerPage={newsPerPage}
+                 setCurrentPage={setCurrentPage}
+                 currentPage={currentPage}
+               />
+             </div>
+           ) : (
+             <div className="mt-5 h-40  ">
+               <p className="text-center font-bold text-xl mt-10">
+                 No news is available at the moment
+               </p>
+               <p className="text-center font-semibold text-xl ">
+                 Please check back later
+               </p>
+             </div>
+           )}
+         </div>
+       )}
+       <div className="mt-5">
+         <Footer />
+       </div>
+     </div>
+   );
 };
 
 export default News;
